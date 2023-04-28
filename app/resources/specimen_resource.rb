de3afe6ab955fdd2d69842777f489b3f4c6e6750
Paul D Mitchell <paul.d.mitchell@ed.ac.uk>
2023-04-28 10:03:48 +0100

    Add additional attributes

--- app/resources/specimen_resource.rb
+++ app/resources/specimen_resource.rb
@@ -3,4 +3,10 @@ class SpecimenResource < ApplicationResource
   attribute :barcode, :string
   attribute :specimen_class, :string
   attribute :specimen_type, :string
+  attribute :comments, :string, sortable: false
+  attribute :born_on, :date
+  attribute :status, :string
+  attribute :site, :string
+  attribute :side, :string
+  attribute :availability, :string
 end
