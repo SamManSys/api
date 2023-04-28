515aa051eb6e7f30dac59cf6103aefdf7e37da6f
Paul D Mitchell <paul.d.mitchell@ed.ac.uk>
2023-04-28 10:01:15 +0100

    Add specimen factory

---
+++ spec/factories/specimen.rb
@@ -0,0 +1,30 @@
+FactoryBot.define do
+  factory :specimen do
+    sequence(:label) { |n| "ZZ#{n}" }
+    sequence(:barcode) { |n| "#{n}" }
+    availability { 'available' }
+    status { :active }
+
+    trait :closed do
+      status { :closed }
+    end
+
+    trait :disabled do
+      status { :disabled }
+    end
+    
+    trait :unavailable do
+      availability { 'not_available' }
+    end
+
+    trait :tissue do
+      specimen_class { "Fresh" }
+      specimen_type  { "Tissue" }
+    end
+
+    trait :fluid do
+      specimen_class { "Fluid" }
+      specimen_type  { "Blood" }
+    end
+  end
+end
