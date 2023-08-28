7a6ff0c584c7976480ad5908793837146aa5ca05
Paul D Mitchell <paul.d.mitchell@ed.ac.uk>
2023-08-28 11:44:32 +0100

    Simpilfy sample def

--- app/resources/sample_resource.rb
+++ app/resources/sample_resource.rb
@@ -1,21 +1,6 @@
-class SampleResource < ApplicationResource
+class SampleResource < SpecimenResource
   self.model = Specimen
   self.type = :samples
-  
-  belongs_to :specimen_collection 
-  primary_endpoint '/samples', [:index, :show, :create, :update, :destroy]
 
-  attribute :label, :string
-  attribute :barcode, :string
-  attribute :specimen_class, :string
-  attribute :specimen_type, :string
-  attribute :comments, :string, sortable: false
-  attribute :born_on, :date
-  attribute :status, :string
-  attribute :site, :string
-  attribute :side, :string
-  attribute :availability, :string
-  attribute :available_quantity, :float
-  
-  attribute :specimen_collection_id, :integer, only: [:filterable]
+  primary_endpoint '/samples', [:index, :show, :create, :update, :destroy]
 end
